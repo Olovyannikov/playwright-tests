ef5504a91ee45d2b63da60e446e6324e5c5511e8
Ilya Olovyannikov <excaliburgyro@gmail.com>
2024-12-13 20:18:20 +0300

    feat(store): add store tests

--- core/api/store-api.ts
+++ core/api/store-api.ts
@@ -1,26 +1,60 @@
+import { expectStatusCode } from '@/utils/assertions/solutions';
+import { APIRoutes } from '@/utils/constants/routes';
+import { APIClient } from '@/utils/types/api/client';
+import type { Order } from '@/utils/types/api/types/order';
+
 import test, { APIRequestContext, APIResponse } from '@playwright/test';
-import { APIClient } from '../../utils/types/api/client';
-import { APIRoutes } from '../../utils/constants/routes';
-import { Order } from '../../utils/types/api/types/order';
 
 export class StoreApiClient implements APIClient {
     constructor(public context: APIRequestContext) {}
 
     async getOrderByIdAPI(orderId: number): Promise<APIResponse> {
         return await test.step(`Getting current order by "${orderId}"`, async () => {
-            return await this.context.get(`${APIRoutes.STORE}/${orderId}`);
+            return await this.context.get(`v2${APIRoutes.STORE}/${orderId}`);
         });
     }
 
-    async createOrderAPI(order: Order): Promise<APIResponse> {
+    async createOrderAPI(order: Partial<Order>): Promise<APIResponse> {
         return await test.step(`Create order`, async () => {
-            return await this.context.post(`${APIRoutes.STORE}}`, { data: order });
+            return await this.context.post(`v2${APIRoutes.STORE}`, { data: order });
         });
     }
 
     async deleteOrderAPI(orderId: number): Promise<APIResponse> {
         return await test.step(`Delete current order by "${orderId}"`, async () => {
-            return await this.context.delete(`${APIRoutes.STORE}/${orderId}`);
+            return await this.context.delete(`v2${APIRoutes.STORE}/${orderId}`);
         });
+    }
+
+    async getOrderById(orderId: number): Promise<APIResponse> {
+        const response = await this.getOrderByIdAPI(orderId);
+        await expectStatusCode({ actual: response.status(), expected: 200, api: response.url() });
+        return await response.json();
+    }
+
+    async createOrder(order: Order): Promise<APIResponse> {
+        const response = await this.createOrderAPI(order);
+        await expectStatusCode({ actual: response.status(), expected: 200, api: response.url() });
+        return await response.json();
+    }
+
+    async createInvalidOrder(order: Partial<Order>) {
+        const response = await this.createOrderAPI(order);
+        await expectStatusCode({ actual: response.status(), expected: 500, api: response.url() });
+    }
+
+    async deleteOrder(order: number) {
+        const response = await this.deleteOrderAPI(order);
+        await expectStatusCode({ actual: response.status(), expected: 200, api: response.url() });
+    }
+
+    async checkDeletedOrder(orderId: number) {
+        const response = await this.deleteOrderAPI(orderId);
+        await expectStatusCode({ actual: response.status(), expected: 404, api: response.url() });
+    }
+
+    async getNonExistedOrders() {
+        const response = await this.getOrderByIdAPI(0);
+        await expectStatusCode({ actual: response.status(), expected: 404, api: response.url() });
     }
 }
---
+++ tests/store.spec.ts
@@ -0,0 +1,54 @@
+import { StoreApiClient } from '@/core/api/store-api';
+import { getDefaultAPIContext } from '@/core/context/default-context';
+import { Order } from '@/utils/types/api/types/order';
+
+import test, { expect } from '@playwright/test';
+
+test.describe('Store', async () => {
+    test('Get Order By Id', async () => {
+        const context = await getDefaultAPIContext();
+        const storeApiClient = new StoreApiClient(context);
+        await storeApiClient.getOrderById(2);
+    });
+
+    test('Create Order', async () => {
+        const context = await getDefaultAPIContext();
+        const storeApiClient = new StoreApiClient(context);
+
+        const newOrder = await storeApiClient.createOrder({
+            id: 1,
+            petId: 12345,
+            quantity: 1,
+            shipDate: '2024-09-30T13:00:00.000Z',
+            status: 'placed',
+            complete: false,
+        });
+
+        expect(newOrder.status).toEqual('placed');
+    });
+
+    test('Invalid Order', async () => {
+        const context = await getDefaultAPIContext();
+        const storeApiClient = new StoreApiClient(context);
+
+        await storeApiClient.createInvalidOrder({
+            id: 'invalid_id',
+            petId: 'invalid_petId',
+            quantity: -1,
+            status: 'unknown',
+        } as unknown as Order);
+    });
+
+    test('Delete Order', async () => {
+        const context = await getDefaultAPIContext();
+        const storeApiClient = new StoreApiClient(context);
+        await storeApiClient.deleteOrder(1);
+        await storeApiClient.checkDeletedOrder(1);
+    });
+
+    test('Order not found', async () => {
+        const context = await getDefaultAPIContext();
+        const storeApiClient = new StoreApiClient(context);
+        await storeApiClient.getNonExistedOrders();
+    });
+});
